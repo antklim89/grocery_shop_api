ede1f22e0a23124fe327f7194d0721e9791b16e5
antklim89 <antklim89@mail.ru>
2021-07-21 00:38:11 +0000

    add category and country routes

---
+++ api/category/controllers/category.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+    async enumerate(ctx) {
+        const result = await strapi
+            .query('category')
+            .find()
+
+        return result.map((i) => i.name)
+    }
+};
