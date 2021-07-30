8a5ffbb4837e554adaf909a9a01b3d421673ed38
Anton <antklim89@mail.ru>
2021-07-30 23:15:37 +0000

    update

--- api/cart/controllers/cart.js
+++ api/cart/controllers/cart.js
@@ -1,8 +1,36 @@
 'use strict';
 
+const { sanitizeEntity } = require("strapi-utils");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async find(ctx) {
+        const { user } = ctx.state;
+
+        const entities = ctx.query._q
+            ? await strapi.services.cart.search({...ctx.query, user: user.id})
+            : await strapi.services.cart.find({...ctx.query, user: user.id});
+
+        return entities.map((entity) => sanitizeEntity(entity, { model: strapi.models.cart }))
+    },
+
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        const { user } = ctx.state;
+    
+        const entity = await strapi.services.cart.findOne({ id, user: user.id });
+        return sanitizeEntity(entity, { model: strapi.models.cart });
+    },
+
+    async create(ctx) {
+        const entity = await strapi.services.cart.create({
+          ...ctx.request.body,
+          user: ctx.state.user.id,
+        });
+        return sanitizeEntity(entity, { model: strapi.models.cart });
+      },
+};
--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -8,21 +8,20 @@ module.exports = {
    */
 
   async findOne(ctx) {
-    const { id } = ctx.params;
-    const { user } = ctx.state;
+      const { id } = ctx.params;
+      const { user } = ctx.state;
 
-    const entity = await strapi.services.order.findOne({ id, user: user.id });
-    return sanitizeEntity(entity, { model: strapi.models.order });
+      const entity = await strapi.services.order.findOne({ id, user: user.id });
+      return sanitizeEntity(entity, { model: strapi.models.order });
   },
 
   async create(ctx) {
-    console.debug('||ctx.request.body: \n', ctx.request.body);
-    const entity = await strapi.services.order.create({
-      ...ctx.request.body,
-      user: ctx.state.user.id,
-      uid: Math.random().toString(32),
-    });
-    return sanitizeEntity(entity, { model: strapi.models.order });
+      const entity = await strapi.services.order.create({
+          ...ctx.request.body,
+          user: ctx.state.user.id,
+          uid: Math.random().toString(32),
+      });
+      return sanitizeEntity(entity, { model: strapi.models.order });
   },
 };
  
