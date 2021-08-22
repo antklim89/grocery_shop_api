58732263ffe82c6ccabe5940395399e31ef6f757
Anton <31469798+antklim89@users.noreply.github.com>
2021-08-22 08:39:00 +0000

    devdata

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -22,56 +22,58 @@ module.exports = {
             'SELECT id FROM upload_file;',
         );
 
-        await strapi.services.order.delete({});
-        await strapi.services.cart.delete({});
+        // console.debug(': \n', images)
 
-        await strapi.services.category.delete({});
-        const categories = await Promise.all(categoryList.map((category) => (
-            strapi.services.category.create({ name: category })
-        )));
+        // await strapi.services.order.delete({});
+        // await strapi.services.cart.delete({});
 
-        await strapi.services.country.delete({});
-        const countries = await Promise.all(countryList.map((country) => (
-            strapi.services.country.create({ name: country })
-        )));
+        // await strapi.services.category.delete({});
+        // const categories = await Promise.all(categoryList.map((category) => (
+        //     strapi.services.category.create({ name: category })
+        // )));
 
-        await strapi.services.product.delete({});
-        await Promise.all(productList.map((product) => (
-            strapi.services.product.create({
-                name: product,
-                description: faker.commerce.productDescription(),
-                price: _.random(10, 100, false),
-                discount: _.random(0, 100) > 50 ? _.random(0, 80, false) : 0,
-                unit: _.sample(unit),
-                quantityPerUnit: _.sample(quantityPerUnit),
-                country: _.sample(_.map(countries, 'id')),
-                category: _.sample(_.map(categories, 'id')),
-                mainImage: _.sample(images),
-                images: _.sampleSize(_.map(images, 'id'), 3),
-            })
-        )));
+        // await strapi.services.country.delete({});
+        // const countries = await Promise.all(countryList.map((country) => (
+        //     strapi.services.country.create({ name: country })
+        // )));
 
-        await strapi.services.feature.delete({});
-        await Promise.all(featureList.map((feature) => (
-            strapi.services.feature.create({
-                title: feature,
-                feature: faker.lorem.sentences(5),
-                image: _.sample(_.map(images, 'id')),
-            })
-        )));
+        // await strapi.services.product.delete({});
+        // await Promise.all(productList.map((product) => (
+        //     strapi.services.product.create({
+        //         name: product,
+        //         description: faker.commerce.productDescription(),
+        //         price: _.random(10, 100, false),
+        //         discount: _.random(0, 100) > 50 ? _.random(0, 80, false) : 0,
+        //         unit: _.sample(unit),
+        //         quantityPerUnit: _.sample(quantityPerUnit),
+        //         country: _.sample(_.map(countries, 'id')),
+        //         category: _.sample(_.map(categories, 'id')),
+        //         mainImage: _.sample(images),
+        //         images: _.sampleSize(_.map(images, 'id'), 3),
+        //     })
+        // )));
 
-        await strapi.services.about.createOrUpdate({
-            title: 'About Us',
-            body: faker.lorem.sentences(7),
-            image: _.sample(_.map(images, 'id')),
-        });
+        // await strapi.services.feature.delete({});
+        // await Promise.all(featureList.map((feature) => (
+        //     strapi.services.feature.create({
+        //         title: feature,
+        //         feature: faker.lorem.sentences(5),
+        //         image: _.sample(_.map(images, 'id')),
+        //     })
+        // )));
 
-        await strapi.services.hero.createOrUpdate({
-            title: '100% NATURAL!',
-            text: faker.lorem.sentences(7),
-            image: _.sample(_.map(images, 'id')),
-            secondText: 'fresh & non-gmo products',
-        });
+        // await strapi.services.about.createOrUpdate({
+        //     title: 'About Us',
+        //     body: faker.lorem.sentences(7),
+        //     image: _.sample(_.map(images, 'id')),
+        // });
+
+        // await strapi.services.hero.createOrUpdate({
+        //     title: '100% NATURAL!',
+        //     text: faker.lorem.sentences(7),
+        //     image: _.sample(_.map(images, 'id')),
+        //     secondText: 'fresh & non-gmo products',
+        // });
 
         return true;
     },
